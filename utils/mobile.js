a682bf0d6af1313842c78aeb96599b32bfc53742
Jeremy Weinstein <jeremydw@gmail.com>
2016-03-24 12:38:41 -0700

    Initial commit of utils.mobile.

---
+++ utils/mobile.js
@@ -0,0 +1,33 @@
+/**
+ * @fileoverview Utility functions related to mobile devices.
+ */
+
+
+function isIOS() {
+  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
+
+
+function isAndroid() {
+  return /Android/i.test(navigator.userAgent);
+}
+
+
+function isIE() {
+  return /MSIE\/\d+/.test(navigator.userAgent);
+}
+
+
+function isIEorEdge() {
+  return /Edge\/\d+/.test(navigator.userAgent) ||
+      /MSIE\/\d+/.test(navigator.userAgent) ||
+      /Trident\/\d+/.test(navigator.userAgent);
+}
+
+
+module.exports = {
+  isAndroid: isAndroid,
+  isIOS: isIOS,
+  isIE: isIE,
+  isIEorEdge: isIEorEdge
+};
