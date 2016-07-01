63fff24b50f045d9202f1512320f30c515fa0abc
Jeremy Weinstein <jeremydw@gmail.com>
2016-07-01 15:41:26 -0700

    Move isElementInView to ui root.

--- ui/inview.js
+++ ui/inview.js
@@ -5,32 +5,7 @@
 
 var classes = require('../utils/classes');
 var scrolldelegator = require('../scrolldelegator');
-
-
-/**
- * Returns whether an element is visibile in the viewport.
- * @param {Element} el Element to check.
- * @param {number=} opt_offset Offset to apply when checking
- *    whether the element is in view. The value of opt_offset should
- *    be a fraction (e.g. 0.85) and the resulting offset value is
- *    determined by multiplying opt_offset with the element's height.
- * @return {boolean} Whether the element is in view.
- */
-function isElementInView(el, opt_offset) { 
-  var rect = el.getBoundingClientRect();
-  var root = document.documentElement;
-  var height = rect.bottom - rect.top;
-  var offset = 0;
-  if (opt_offset) {
-    offset = height * opt_offset;
-  }
-  return (
-    rect.top + offset >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom - offset <= (window.innerHeight || root.clientHeight) &&
-    rect.right <= (window.innerWidth || root.clientWidth)
-  );
-}
+var ui = require('../ui');
 
 
 /**
@@ -61,7 +36,7 @@ InViewClassAdder.DefaultConfig = {
 InViewClassAdder.prototype.onScroll = function() {
   var els = document.querySelectorAll(this.selector);
   [].forEach.call(els, function(el) {
-    if (isElementInView(el, this.offset)) {
+    if (ui.isElementInView(el, this.offset)) {
       if (this.delay) {
         var min = this.delay[0];
         var max = this.delay[1];
@@ -102,6 +77,5 @@ function addClassInView(config) {
 
 
 module.exports = {
-  addClassInView: addClassInView,
-  isElementInView: isElementInView
+  addClassInView: addClassInView
 };
